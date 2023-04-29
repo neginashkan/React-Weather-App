579bc9629797f449a05bad35dd22ca9e0b6cfe48
negnq <117580177+neginashkan@users.noreply.github.com>
2023-04-29 06:23:17 +0430

    add open-source-code

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <div className='App'>
       <Search/>
       <Weather />
+      <div className='open-source-code'><a href="https://github.com/neginashkan/React-Weather-App" target="_blank" rel="noopener noreferrer">open source code</a> by Negin ashkan</div>
     </div>
   )
 }
