3a31c8b145ee43f56a9e8452cf33cd12008cc6ff
negnq <117580177+neginashkan@users.noreply.github.com>
2023-05-06 15:04:39 +0430

    update date

--- src/components/DateFormat.jsx
+++ src/components/DateFormat.jsx
@@ -2,11 +2,12 @@ function Date(props) {
     const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const hour = props.date.getHours()
+    const minutes = props.date.getSeconds()
     return (<div>
         <div className="date">{props.date.getDate()}th {month[props.date.getMonth()]} {props.date.getFullYear()}</div>
         <ul>
             <li>{dayNames[props.date.getDay()]}</li>
-            <li> {hour < 10 ? `0${hour}`:hour}:{props.date.getSeconds()}</li>
+            <li> {hour < 10 ? `0${hour}` : hour} : {minutes < 10 ? `0${minutes}` : minutes}</li>
         </ul>
     </div>)
 }
