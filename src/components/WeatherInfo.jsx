ea43b1d67040a0bd04d1b14cf6d89ba4dfdccb42
negnq <117580177+neginashkan@users.noreply.github.com>
2023-05-07 07:55:35 +0430

    create WeatherTemperature componenet

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,35 +1,13 @@
 import DateFormat from "./DateFormat"
+import WeatherTemperature from "./WeatherTemperature"
 import Day from "./Day"
-import { useState } from "react";
 function WeatherInfo(props) {
-    const [showCelsius, setShowCelsius] = useState(true)
-    function handleClick(event) {
-        setShowCelsius(!event.target.checked)
-    }
     return (
         <div className="Weather">
             <div className="location"><i class="fa-solid fa-location-dot"></i> {props.info.country} ,{props.info.city}</div>
             <img className="big-icon" title={props.info.description} alt={props.info.description} src={props.info.iconUrl} />
             <div className="description">{props.info.description}</div>
-            <div className="temperature">
-                <h1 className="degree">{showCelsius ? props.info.celsius : Math.round((props.info.celsius * 9 / 5) + 32)}<span className="unit">° {showCelsius ? "C" : "F"}</span></h1>
-                <div className="converter">
-                    <form>
-                        <input
-                            id="switch-button"
-                            name="switch-button"
-                            type="checkbox"
-                            className="input-checkbox"
-                            onClick={handleClick}
-                        />
-                        <label htmlFor="switch-button" className="toggle">
-                            toggle
-                        </label>
-                        <span className="light--text label-text">F</span>
-                        <span className="dark--text label-text">C</span>
-                    </form>
-                </div>
-            </div>
+            <WeatherTemperature celsius={props.info.celsius}/>
             <DateFormat date={props.info.date} />
             <ul>
                 <li><i class="fa-sharp fa-solid fa-wind"></i> Wind {props.info.wind} km/h</li>
@@ -44,7 +22,7 @@ function WeatherInfo(props) {
                     <Day />
                     <Day /> */}
             </div>
-            <div className='open-source-code'><a href="https://github.com/neginashkan/React-Weather-App" target="_blank" rel="noopener noreferrer">open source code</a> by Negin ashkan</div>
+            <div className='open-source-code'><a href="https://github.com/neginashkan/React-Weather-App" target="_blank" rel="noopener noreferrer">open source code</a> by Negin ashkan and <a href="https://github.com/neginashkan/React-Weather-App" target="_blank" rel="noopener noreferrer">hosted on Netlify</a> </div>
         </div>
     )
 }
